1f0deddcd866e0364cb523ab126182d4b47e2eb6
Leo Alciso <leoalciso@Leos-MacBook-Air.local>
2021-11-15 14:23:30 -0800

    button routing and views

--- garden/gardenApp/models.py
+++ garden/gardenApp/models.py
@@ -1,21 +1,14 @@
 from django.db import models
 
-
 # Create your models here.
 class PublicUser(models.Model):
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=64)
     pass_hash = models.CharField(max_length=128)
-    #address = models.CharField(max_length=256)
+    address = models.CharField(max_length=256)
 
     def __repr__(self):
         return str(self.__dict__)
-
-
-class CorpUser(models.Model):
-    email = models.EmailField()
-    pass_hash = models.CharField(max_length=128)
-    address = models.CharField(max_length=128)
 
 
 class Produce(models.Model):
@@ -26,7 +19,7 @@ class Produce(models.Model):
 
 class Donation(models.Model):
     produce_id = models.ForeignKey(Produce, on_delete=models.CASCADE)  # foreign key
-    donatee = models.ForeignKey(CorpUser, on_delete=models.CASCADE)  # foreign key
+    donatee = models.ForeignKey(PublicUser, on_delete=models.CASCADE)  # foreign key
     date_created = models.DateField(auto_now_add=True)  # date time
 
 
@@ -38,7 +31,8 @@ class ProduceAlert(models.Model):
 class ProduceRequest(models.Model):
     produce_name = models.CharField(max_length=64)
     weight = models.DecimalField(max_digits=5, decimal_places=2)
-    corp_user = models.ForeignKey(CorpUser, on_delete=models.CASCADE)  # foreign key
+    user = models.ForeignKey(PublicUser, on_delete=models.CASCADE)  # foreign key
+
 
 class Article(models.Model):
     title = models.CharField(max_length=64)
