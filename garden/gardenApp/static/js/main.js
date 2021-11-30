0df8740d70af714923b5382d7677ffb5f00eda3e
Jobelle-Abellera <57641810+Jobelle-Abellera@users.noreply.github.com>
2021-11-30 11:41:09 -0800

    Small styling changes
    We should check if the picture uploading still works
    - File upload now has name of file (createpost, editpost, profile)
    - Footer updated (all pages)
    - Features updated (Homepage)

--- garden/gardenApp/static/js/main.js
+++ garden/gardenApp/static/js/main.js
@@ -1,9 +1,12 @@
-// make header stretch across the screen width
-var ready = (callback) => {
-    if (document.readyState != "loading") callback();
-    else document.addEventListener("DOMContentLoaded", callback);
-}
-ready(() => {
-    document.querySelector(".header").style.height = window.innerHeight + "px";
+// update file name after uploading a file
+// idea: https://stackoverflow.com/questions/572768/styling-an-input-type-file-button
+$('#myfile').bind('change', function() { 
+	var fileName = ''; 
+	fileName = $(this).val(); 
+	var span = document.getElementById("file-selected");
+//	$('#file-selected').innerHTML(fileName); 
+	var typeOut = fileName.split("\\").pop();	// https://stackoverflow.com/questions/1804745/get-the-filename-of-a-fileupload-in-a-document-through-javascript
+	if (typeOut == "") typeOut = "No file selected";
+	span.innerHTML = typeOut;
+	//console.log(fileName);
 })
-
